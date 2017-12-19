cad80d03bed8ba384ce2cc896828ed1ce1e38c9c
DaniloGiacchi <34310129+DaniloGiacchi@users.noreply.github.com>
2017-12-19 20:45:14 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,22 +73,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 #this is used to blind a postgres_db
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'websitedb',
-        'USER': 'postgres',
-        'PASSWORD': 'danilog24',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 #DATABASES = {
  #   'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- #   }
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': 'websitedb',
+    #    'USER': 'postgres',
+     #   'PASSWORD': 'danilog24',
+      #  'HOST': 'localhost',
+       # 'PORT': '5432',
+    #}
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
